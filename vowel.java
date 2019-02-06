b3dd0794a99ae89d9ee88875082d19444cee2a11
veerasinnammal <47377785+veerasinnammal@users.noreply.github.com>
2019-02-06 14:39:00 +0530

    Create vowel.java

---
+++ vowel.java
@@ -0,0 +1,51 @@
+import java.io.*;
+public class vowel
+
+{
+	
+public static void main (String[] args) 
+	
+{
+	
+ char a='a',e='e',i='i',o='o',u='u';
+
+switch(a)
+
+{
+
+case 'a':
+
+System.out.println("a is vowel");
+
+break;
+
+case 'e':
+
+System.out.println("e is vowel");
+
+break;
+
+case 'i':
+
+System.out.println("i is vowel");
+
+break;
+
+case 'o':
+
+System.out.println("o is vowel");
+
+break;
+
+case 'u':
+
+System.out.println("u is vowel");
+
+break;
+
+default:
+
+System.out.println("Not A Vowel");
+
+}
+	
