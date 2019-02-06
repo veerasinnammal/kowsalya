34eb67b748e25558ef8846b772160ada6daddf87
veerasinnammal <47377785+veerasinnammal@users.noreply.github.com>
2019-02-06 14:53:46 +0530

    Create alphabet.java

---
+++ alphabet.java
@@ -0,0 +1,15 @@
+import java.io.*;
+public class Alphabet
+    {
+
+    public static void main(String[] args)
+    {
+
+        char c = 'a';
+
+        if( (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+            System.out.println(c + " is an alphabet.");
+        else
+            System.out.println(c + " is not an alphabet.");
+    }
+}
