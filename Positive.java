1bd88d78ccb61d4d9919ae98cdec698f9cc8f741
veerasinnammal <47377785+veerasinnammal@users.noreply.github.com>
2019-02-06 12:10:01 +0530

    Create Positive.java

---
+++ Positive.java
@@ -0,0 +1,44 @@
+
+	
+	
+	
+
+import java.io.*;
+
+ class PositiveNagative
+
+{
+   
+ public static void main(String []args)
+ 
+   {
+    
+int n=3;
+  
+  if(n==3)
+    
+{
+   
+ System.out.println("Postive");
+  
+  }
+ 
+   else if(n==-3)
+ 
+   {
+        System.out.println("Negative");
+
+    }
+ 
+   else if(n==0)
+ 
+   {
+       
+ System.out.println("Zero");
+  
+  }
+ 
+   }
+}
+
+
