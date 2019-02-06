95f6833c6cc99195a623b4848ec09ec5df7ac009
veerasinnammal <47377785+veerasinnammal@users.noreply.github.com>
2019-02-06 13:47:07 +0530

    Create oddeven.java

---
+++ oddeven.java
@@ -0,0 +1,29 @@
+import java.io.*;
+
+public class evenodd
+
+{
+	
+public static void main (String[] args) 
+	
+{
+	
+int n=3;
+	
+if(n==3)
+	
+{
+	    
+System.out.println("Odd");
+	
+}
+	
+else
+	
+{
+	   
+ System.out.println("Even");
+	
+}
+	
+}
